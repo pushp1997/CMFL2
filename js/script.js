ffc9da685a7ea2e8ab2ed53c9da722149c49f76a
Pushp Vashisht <pushptyagi1@gmail.com>
2019-07-25 22:38:56 +0530

    Final Commit: everything working

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ result = null;
 rawEle = null;
 cenEle = null;
 imgSrc = null;
-
+emoji = document.getElementsByClassName('overlay');
 function callbackLoad() {
     console.log('callback after face detection model is loaded!');
 }
@@ -12,24 +12,30 @@ function callbackLoad() {
 function callbackPredict(err, results) {
     result = results;
     cenEle = document.getElementById('censoredImage');
-    if(result['outputs'][0].score > 0.7){
-        cenEle.setAttribute('src', imgSrc);
-        cenEle.classList.remove("hide");
-        x = result["outputs"][0].box.x;
-        y = result["outputs"][0].box.y;
-        h = result["outputs"][0].box.height;
-        w = result["outputs"][0].box.width;
-        overlay = document.getElementById('overlay');
+    cenEle.setAttribute('src', imgSrc);
+    cenEle.classList.remove("hide");
+    for(i=0;i<results['outputs'].length;i++){
+        overlay = document.createElement("i");
+        overlay.classList.add('overlay','em','hide');
+        x = result["outputs"][i].box.x;
+        y = result["outputs"][i].box.y;
+        h = result["outputs"][i].box.height;
+        w = result["outputs"][i].box.width;
         overlay.style.transform="translate("+x+"px,"+y+"px)";
-        overlay.classList.remove("hide");
         overlay.style.height = h+'px';
         overlay.style.width = w+'px';
-    }else{
-        console.log('score not greater than 0.7');
-        alert('Face detection score, lower than 70%');
+        overlay.classList.remove("hide");
+        document.getElementById('container').appendChild(overlay);
+    }
+    clickListener();
+}
+function clickListener(){
+    for(i=0; i<emoji.length; i++){
+        emoji[i].addEventListener("click",function(){
+            this.classList.toggle("em-angry");
+        });
     }
 }
-
 async function detectFaces(){
     await stackml.init({'accessKeyId': '15e4d1331af8253eaa30c13b65ba7252'});
     // load face detection model
